5a9362b28e4c4ddd0d909a469f50659889c396d6
Mr Zhu <50517734+banghyuu@users.noreply.github.com>
2019-05-16 18:19:57 +0000

    Add files via upload

---
+++ pelet.sh
@@ -0,0 +1,187 @@
+#!/usr/bin/bash
+#IndonesianHWT
+#caranembakcewek
+#by MrZhu
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|=======================================|>"
+echo  $green    "|         TOOL BUAT HACKER JOMBLO         |"
+echo  $green    "|                                         |"
+echo  $green    "| Author        : Mr Zhu                  |"
+echo  $green    "| Contact       : 0859175727510 	   |"
+echo  $green    "| My Team       : Indonesian Hazyweb Team |"
+echo  $green    "| Thanks To     : Allah SWT               |"
+echo  $green    "| My Love       : Masih Mencari :v	   |"
+echo  $blue "<|=======================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Tembak Cewek Yang Baik Hati  $blue ||"
+echo $blue "|| $green 2. Tembak cewek Yang Cuek       $blue ||"
+echo $blue "|| $green 3. Tembak Cewek Yang Pemalu     $blue ||"
+echo $blue "|| $green 4. Tembak Cewek Yang Matre      $blue ||"
+echo $blue "|| $green 0. Exit       		     $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Pilih Yang Mana]>"
+read -p"╰───────root@mrzhu404=>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Namamu Siapa]>"
+read -p"╰───────root@mrzhu404=•>" nama;
+sleep 1
+echo $green"╭──[Nama Ceweknya]>"
+read -p"╰───────root@mrzhu404=•>" cewek;
+sleep 1
+echo $green "Bismillah"
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Alhamdulillah"
+sleep 1
+echo  $blue "Semoga kamu berhasil wahai $nama untuk dapetin $cewek yang baik hati ini"
+fi
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+echo $blue"╭──[Namamu Siapa]>"
+read -p"╰───────root@mrzhu404=>" nama;
+sleep 1
+echo $green"╭──[Nama Ceweknya]>"
+read -p"╰───────root@mrzhu404=>" cewek;
+sleep 1
+echo $green "Bismillah"
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Alhamdulillah"
+sleep 1
+echo  $blue "Semoga kamu berhasil wahai $nama untuk dapetin $cewek yang cuek ini :v"
+fi
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Namamu Siapa]>"
+read -p"╰───────root@mrzhu404=>" nama;
+sleep 1
+echo $green"╭──[Nama Ceweknya]>"
+read -p"╰───────root@mrzhu404=>" cewek;
+sleep 1
+echo $green "Bismillah"
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Alhamdulillah"
+sleep 1
+echo  $blue "Semoga kamu berhasil wahai $nama untuk dapetin $cewek yang pemalu ini :) "
+fi
+
+if [ $Gans = 4 ] || [ $Gans = 04 ]
+then
+echo $blue"╭──[Namamu Siapa]>"
+read -p"╰───────root@mrzhu404=>" nama;
+sleep 1
+echo $green"╭──[Nama Ceweknya]>"
+read -p"╰───────root@mrzhu404=>" cewek;
+sleep 1
+echo $green "Bismillah"
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Alhamdulillah"
+sleep 1
+echo  $blue "Mari kita doakan wahai $nama semoga $cewek cepat mati karena matre :( "
+fi
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "Jangan lupakan nama saya Mr Zhu"
+echo  $blue "Admin dari Indonesian Hazyweb Team!"
+echo  $green "Sampai jumpa!"
+exit
+fi
